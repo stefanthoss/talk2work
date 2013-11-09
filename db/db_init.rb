5715cd855f16c4e8dd9f4de2a61983f2027b4a07
stefanthoss <stef@nthoss.de>
2013-11-09 12:35:09 -0800

    Added carvoyant account, changed database schema to fit Carvoyant API

--- db/db_init.rb
+++ db/db_init.rb
@@ -7,6 +7,14 @@ dbconfig = YAML::load(File.open('config/database.yml'))
 
 client = Mysql2::Client.new(dbconfig)
 
-puts "Create table coordinates..."
-results = client.query("DROP TABLE IF EXISTS coordinates")
-results = client.query("CREATE TABLE coordinates (id INT NOT NULL AUTO_INCREMENT, lat DECIMAL(10,6) NOT NULL, lng DECIMAL(10,6) NOT NULL, PRIMARY KEY (id))")
+puts "Create table cars..."
+results = client.query("DROP TABLE IF EXISTS cars")
+results = client.query("CREATE TABLE cars (id INT NOT NULL, name VARCHAR(255), PRIMARY KEY (id))")
+
+puts "Create table trips..."
+results = client.query("DROP TABLE IF EXISTS trips")
+results = client.query("CREATE TABLE trips (id INT NOT NULL, start_lat DECIMAL(10,6) NOT NULL, start_lng DECIMAL(10,6) NOT NULL, end_lat DECIMAL(10,6) NOT NULL, end_lng DECIMAL(10,6) NOT NULL, PRIMARY KEY (id))")
+
+# puts "Create table coordinates..."
+# results = client.query("DROP TABLE IF EXISTS coordinates")
+# results = client.query("CREATE TABLE coordinates (id INT NOT NULL AUTO_INCREMENT, lat DECIMAL(10,6) NOT NULL, lng DECIMAL(10,6) NOT NULL, PRIMARY KEY (id))")
