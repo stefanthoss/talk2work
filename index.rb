1e0f6449968d1ca3908fbf545c7abef09c48c754
Tobias Thiel <tobias011@gmail.com>
2013-11-09 14:01:50 -0800

    fix redirect url

--- index.rb
+++ index.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 
 YAMMER_CLIENT_ID = 'wN7Tyzyeo5eXxXe7MP9U2g'
 YAMMER_CLIENT_SECRET = 'Fk0njVHhYH59TdaXEVOW69yHl8NIbIdohNgTK75w'
-YAMMER_REDIRECT_URI = 'http://localhost/auth/yammer/callback'
+YAMMER_REDIRECT_URI = 'http://192.241.233.188/auth/yammer/callback'
 
 class MyApplication < Sinatra::Base
 
