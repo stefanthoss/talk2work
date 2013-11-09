f18b3a63f40f47375088ed0ebbefa8b919181871
Tobias Thiel <tobias011@gmail.com>
2013-11-09 12:31:59 -0800

    trying out yammer authentication

--- Gemfile
+++ Gemfile
@@ -8,3 +8,5 @@ gem 'puma'
 gem 'shotgun'
 gem "rest-client"
 gem "json"
+gem 'omniauth', '~> 1.0'
+gem 'omniauth-yammer'
--- index.rb
+++ index.rb
@@ -1,5 +1,30 @@
 require 'sinatra'
+require 'omniauth'
+require 'omniauth-yammer'
+require 'rest-client'
 
-get '/?' do
-  'Hello World!'
+YAMMER_CLIENT_ID = 'wN7Tyzyeo5eXxXe7MP9U2g'
+YAMMER_CLIENT_SECRET = 'Fk0njVHhYH59TdaXEVOW69yHl8NIbIdohNgTK75w'
+YAMMER_REDIRECT_URI = 'http://192.241.233.188/auth/yammer/callback'
+
+class MyApplication < Sinatra::Base
+  use Rack::Session::Cookie, :secret => 'Ea2Cie1beir2ChuwahceiPheequees5l'
+  use OmniAuth::Strategies::Yammer, YAMMER_CLIENT_ID, YAMMER_CLIENT_SECRET
+
+  get '/?' do
+    "Hello World! <a href=\"https://www.yammer.com/dialog/oauth?client_id=#{YAMMER_CLIENT_ID}&redirect_uri=#{YAMMER_REDIRECT_URI}\">Login</a>"
+  end
+
+  get '/auth/yammer/callback' do
+    yammer_token = request.env['omniauth.auth']
+    puts "authtoken: #{yammer_token}"
+    network_users = JSON.parse(RestClient.get("https://www.yammer.com/api/v1/users.json", headers: { 'Authorization' => "Bearer #{yammer_token}" }))
+    puts "network_users: #{network_users}"
+    rtnstr = "Your network: #{network_users[0]['network_name']}:<br /><ul>"
+    for user in network_users
+      rtnstr += "<li>#{user['full_name']}</li>"
+    end
+    rtnstr += "</ul>"
+    return rtnstr
+  end
 end
